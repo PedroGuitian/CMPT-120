a2e08d3aebf6b8cdb8aff60df086afeae5108299
PedroGuitian <143512593+PedroGuitian@users.noreply.github.com>
2023-09-26 08:42:52 -0400

    finished

--- Assignment 2/if_else_practice.py
+++ Assignment 2/if_else_practice.py
@@ -5,5 +5,14 @@
 #The else should read: Else print out "10 is less than 10"
 
 def main():
+    number = 10
+    if number > 12:
+        print(str(number) + "is greter than 12")
+    elif number > 11:
+        print(str(number) + "10 is greater tahn 11")
+    elif number == 10:
+        print(str(number) + " is eaul to 10")
+    else:
+        print("10 is ess than 10")
 
 main()
