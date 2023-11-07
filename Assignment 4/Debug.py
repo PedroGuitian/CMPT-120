40696b3043d153221400e358f4a32e8305ed8626
PedroGuitian <143512593+PedroGuitian@users.noreply.github.com>
2023-11-07 08:54:04 -0500

    finished

--- Assignment 4/Debug.py
+++ Assignment 4/Debug.py
@@ -40,6 +40,6 @@ def main():
 
 
 
-    
+
 
 main()
--- Assignment 4/blackjack.py
+++ Assignment 4/blackjack.py
@@ -2,11 +2,20 @@
 #this script is going to require some googling: I want you to practice using your resources with this one. But of course if you get stuck, reach out :)
 '''instructions: randomly generate three values between 1 and 11. in the function bust: add these three numbers together. if they add up to or less than 21, return the sum. If it's over 21, return 0. If it's over 21 BUT there's an 11 as one of the values, return the sum - 10. '''
 
-def bust():
-    
-    
-    
+import random
+def bust(a, b, c):
+    sum = a + b +c
+    if sum <= 21:
+        return(sum)
+    elif sum > 21 and (a == 11 or b == 11 or c == 11):
+        return sum - 10
+    else:
+        return 0
+
 def main():
-    
+    num1 = random.randint(1,11)
+    num2 = random.randint(1,11)
+    num3 = random.randint(1,11)
+    print(bust(num1, num2, num3))
 
 main()
